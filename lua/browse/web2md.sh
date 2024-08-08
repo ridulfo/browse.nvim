35c7666f2a9990a7bcc07a0defa50d7813ac4032
Nicolo <nicoloridulfo@gmail.com>
2024-08-08 21:02:43 +0200

    refactor(web2md): temp file cleanup
    Use a cleanup function to guarantee that the temporary file created
    during the downloading and conversion is deleted, even on error.

--- lua/browse/web2md.sh
+++ lua/browse/web2md.sh
@@ -1,12 +1,19 @@
 set -e  # Exit on error
-URL="$1"
-TEMP_FILE=$(mktemp)  # Creates a temporary file
+
+# Creates a temporary file, this is necessary for pandoc to convert the links
+# from relative to absolute. It will be removed at the end of the script.
+TEMP_FILE=$(mktemp)
+
+# Define a cleanup function to delete the temporary file
+cleanup() {
+  rm -f "$TEMP_FILE"
+}
+
+# Register the cleanup function to be called on script exit
+trap cleanup EXIT
 
 # Download the content with link conversion quietly
-wget -q -k --convert-links -O "$TEMP_FILE" "$URL"
+wget -q -k --convert-links -O "$TEMP_FILE" "$1"
 
 # Process the content with pandoc and grep, then print the output
 cat "$TEMP_FILE" | pandoc -f html -t gfm-raw_html | grep -v '\bdata:image/svg+xml\b'
-
-# Remove the temporary file
-rm "$TEMP_FILE"
